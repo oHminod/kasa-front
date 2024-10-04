51c3fd59e31ce9499e336d1f86c530a9d17edd9c
oHminod <yvesm13@gmail.com>
2024-10-04 15:43:53 +0200

    React loader

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -4,8 +4,16 @@ import HomePage from "../pages/home/homePage";
 import LogementPage from "../pages/logement/logementPage";
 import Layout from "../components/layout";
 import AboutPage from "../pages/about/aboutPage";
+import { useEffect } from "react";
 
 const Router = () => {
+  useEffect(() => {
+    const loadingElement = document.getElementById("loading");
+    if (loadingElement) {
+      loadingElement.style.display = "none";
+    }
+  }, []);
+
   const routes = createBrowserRouter([
     {
       path: "/",
