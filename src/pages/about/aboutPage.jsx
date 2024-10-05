07e032cebece768cc29cf83b1d4138c006d88b88
oHminod <yvesm13@gmail.com>
2024-10-05 04:28:51 +0200

    about page fetches about data instead of hard coding data

--- src/pages/about/aboutPage.jsx
+++ src/pages/about/aboutPage.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import Collapse from "../../components/collapse";
 import Image from "../../components/image";
+import Loading from "../../components/loading";
 
 const AboutPage = () => {
+  const [aboutJSON, setAboutJSON] = useState(null);
+
+  useEffect(() => {
+    const fetchAbout = async () => {
+      const response = await fetch("/data/about.json");
+      const data = await response.json();
+      setAboutJSON(data);
+    };
+
+    fetchAbout();
+  }, []);
+
+  if (!aboutJSON) return <Loading />;
   return (
     <div id="about-page">
       <div className="banner">
@@ -12,26 +27,14 @@ const AboutPage = () => {
         />
       </div>
       <div className="content">
-        <Collapse
-          trigger="Fiabilité"
-          text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
-          rounded={false}
-        />
-        <Collapse
-          trigger="Respect"
-          text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-          rounded={false}
-        />
-        <Collapse
-          trigger="Service"
-          text="La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."
-          rounded={false}
-        />
-        <Collapse
-          trigger="Sécurité"
-          text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-          rounded={false}
-        />
+        {aboutJSON.map((section, i) => (
+          <Collapse
+            key={i}
+            trigger={section.title}
+            text={section.text}
+            rounded={false}
+          />
+        ))}
       </div>
     </div>
   );
